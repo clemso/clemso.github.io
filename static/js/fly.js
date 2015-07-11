d6464bf978bed323264f2474a411f3d85e168310
clemso <clemso@x230i>
2015-07-11 21:23:11 +0200

    New blog engine

--- static/js/fly.js
+++ static/js/fly.js
@@ -1,5 +1,2 @@
 function flyweg(a){document.getElementById(a).style.display="none";return true;}
-var to=Math.floor(0+(80+1)*(Math.random()));var le=Math.floor(0+(75+1)*(Math.random()));document.write("<div id=\"fly\" style=\"top:"+to+"%;position:absolute;left:"+le+"%;\" onmouseover=\"return flyweg('fly')\" ><img src=\"static/img/fly.gif\" /></div>");
-
-
-
+var to=Math.floor(0+(80+1)*(Math.random()));var le=Math.floor(0+(10+1)*(Math.random()));document.write("<div id=\"fly\" style=\"top:"+to+"%;position:fixed;left:"+le+"%;\" onmouseover=\"return flyweg('fly')\" ><img src=\"static/img/fly.gif\" /></div>");
